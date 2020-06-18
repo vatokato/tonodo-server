e6fb972ca6a131f964fd628111bf68c16607aebe
RomanOt <vatokato@gmail.com>
2020-06-18 22:09:54 +0500

    remove users router

--- router/index.js
+++ router/index.js
@@ -4,7 +4,6 @@ const router = express.Router();
 router.use('/tasks', require('./tasks'));
 router.use('/auth', require('./auth'));
 router.use('/reg', require('./reg'));
-router.use('/users', require('./users'));
 router.use('/taskFolders', require('./taskFolders'));
 router.get('/', (req, res) => {
   res.send('todo list api');
